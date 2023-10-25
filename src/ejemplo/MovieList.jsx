0aa9f710e8f01e4a732654b8751b1f4cdadb9fc3
wicho37 <willihernandezramirez@gmail.com>
2023-10-24 22:47:25 -0500

    se agrega la peticion a la api

--- src/ejemplo/MovieList.jsx
+++ src/ejemplo/MovieList.jsx
@@ -1,15 +1,15 @@
 
 
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MoviesList = () => {
   const [datos, setDatos] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  /* const [loading, setLoading] = useState(true);  */
 
   useEffect(() => {
     // se realiza la peticion a la api
-    fetch("https://api.themoviedb.org/3/movie") 
+    fetch('https://api.themoviedb.org/3/movie/11?api_key=880816531571af44aa52a8138da56066') 
       .then((response) => response.json()) // Convierte la respuesta a JSON
       .then((data) => {
           console.log(data)
@@ -21,11 +21,9 @@ const MoviesList = () => {
     <div className='container my-5'>
       <h2>List movies:</h2>
       <hr/>
-      {/* <ul>
-        {datos.map((dato) => (
-          <li key={dato.id}>{dato.nombre}</li>
-        ))}
-      </ul> */}
+      <ul>
+       
+      </ul> 
     </div>
   )
 }
